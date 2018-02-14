5fd928b7faaf98c5cce8709656e0673856db73d6
Aswathy <ashcodes2018@gmail.com>
2018-02-14 09:54:41 -0500

    update

--- controller.js
+++ controller.js
@@ -1,21 +1,21 @@
 (function(){
     function FormController(){
       var vm=this;
-      vm.todoList=[{task:"cook"},{task:"clean"}];//test to see if it adds
+      vm.todoList=[];//test to see if it adds
       vm.addTodo=function(task){
-        todoList.push(task);
+        vm.todoList.push(task);
       }
-      vm.removeTodo=function(index){
-        todoList.splice(index,1);
+      vm.removeTodo=function($index){
+        vm.todoList.splice($index,1);
       }
 //click on x to delete the todo
-      vm.close=function(event){
-        event.stopPropagation();
+      //vm.close=function(event){
+        //event.stopPropagation();
         //($this).parent().remove();
       }
 
-    }
+    
   angular
-      .module("App");
+      .module("App")
       .controller("FormController",FormController)
 })();
